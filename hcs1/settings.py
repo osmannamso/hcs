abb9956921fda69476cbde08f47c9acf81f97939
osmannamso123@gmail.com <osmannamso123@gmail.com>
2019-03-01 18:50:00 +0600

    allow hosts

--- hcs1/settings.py
+++ hcs1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z3zei8n#ngh3&qg2e06$bt3l=vwv$($b+d%i_r-uev$r1u%1um'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
