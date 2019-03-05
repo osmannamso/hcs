fc782a625e45f45e51c668fb2db364cdcf8034c0
osmannamso123@gmail.com <osmannamso123@gmail.com>
2019-03-05 23:07:47 +0600

    adminka

--- house/admin.py
+++ house/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Apartment, Street, House
 
-# Register your models here.
+
+admin.site.register(Apartment)
+admin.site.register(Street)
+admin.site.register(House)
--- house/models.py
+++ house/models.py
@@ -2,15 +2,24 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Street(models.Model):
+    class Meta:
+        verbose_name_plural = 'Улицы'
+        verbose_name = 'Улица'
     name = models.CharField(max_length=50)
 
 
 class House(models.Model):
+    class Meta:
+        verbose_name_plural = 'Дома'
+        verbose_name = 'Дом'
     name = models.CharField(max_length=20)
     street = models.ForeignKey(Street, on_delete=models.CASCADE)
 
 
 class Apartment(models.Model):
+    class Meta:
+        verbose_name_plural = 'Квартиры'
+        verbose_name = 'Квартира'
     name = models.CharField(max_length=20)
     license = models.CharField(max_length=20)
     house = models.ForeignKey(House, on_delete=models.CASCADE)
